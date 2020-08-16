2a797ea01f4548cab54f306ad0797910a43908df
Liam Zdenek <liamzdenek@gmail.com>
2020-08-15 22:13:46 -0500

    Basic header implementation

---
+++ earthquake-zen-garden/src/Containers/Navbar.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {createUseStyles} from 'react-jss';
+import logo from './realtor-com.png';
+import {Link} from 'react-router-dom';
+
+const useStyles = createUseStyles({
+	header: {
+		backgroundColor: "#ddd",
+		padding: "10px",
+		display: "flex",
+		maxHeight: "50px",
+	},
+	logo: {
+		flex: "0 0 auto",
+		maxWidth: "50px",
+		"& >img": {
+			maxHeight: "100%"
+		}
+	},
+	title: {
+		flex: "1 1 auto",
+		textAlign: "center",
+	},
+	userLink: {
+		flex: "0 0 auto",
+	}
+});
+
+export const Navbar: React.FunctionComponent<{}> = () => {
+	const classes = useStyles();
+
+	return (
+		<header className={classes.header}>
+			<div className={classes.logo}>
+				<img src={logo} alt="Realtor.com logo"/>
+			</div>
+			<span className={classes.title}>
+				Earthquake Zen Garden
+			</span>
+			<div className={classes.userLink}>
+				<Link to="/user/sally">
+					Welcome, Sally
+				</Link>
+			</div>
+		</header>
+	);
+}
